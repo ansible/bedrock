ec42d19e780cb1be2b6b93f965430a9255511204
Matt Clay <matt@mystile.com>
2022-05-31 12:56:57 -0700

    Move sync into Python program.

---
+++ sync.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""Sync containers using skopeo."""
+
+import pathlib
+import shlex
+import subprocess
+
+
+def main() -> None:
+    """Main entry point."""
+    name = 'skopeo-sync'
+    image = 'quay.io/skopeo/stable:v1.7.0'
+    sync = [
+        'sync',
+        '--authfile', '/auth.json',
+        '--src', 'yaml',
+        '--dest', 'docker',
+        '--all',
+        '--keep-going',
+        '/sync.yml',
+        'quay.io/bedrock',
+    ]
+
+    try:
+        docker('create', '--rm', '-t', '--name', name, image, *sync)
+        docker('cp', str(pathlib.Path("auth.json").absolute()), f'{name}:/auth.json')
+        docker('cp', str(pathlib.Path("sync.yml").absolute()), f'{name}:/sync.yml')
+        docker('start', '--attach', name)
+    finally:
+        docker('rm', '-f', name)
+
+
+def docker(*args: str) -> None:
+    """Run a docker command."""
+    cmd = ('docker',) + args
+    print(f'==> {shlex.join(cmd)}')
+    subprocess.run(cmd, check=True)
+
+
+if __name__ == '__main__':
+    main()
